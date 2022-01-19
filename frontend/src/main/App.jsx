d601a982b8c92c992a958f85eb3151ba956b77fe
Mauricio-Amr <75991797+Mauricio-Amr@users.noreply.github.com>
2022-01-18 21:22:34 -0300

    import routes

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -8,12 +8,16 @@ import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
 import Main from '../components/template/Main'
 import Footer from '../components/template/Footer'
-import Home from '../components/Home/Home'
+import Routes from './Routes'
+import { HashRouter } from 'react-router-dom'
 
 export default props =>
+
+<HashRouter>
 <div className="app">
     <Logo/>
     <Nav/>
-    <Home/>
+    <Routes/>
     <Footer/>
 </div>
+</HashRouter>
---
+++ frontend/src/main/Routes.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Routes, Route} from "react-router-dom";
+
+
+import Home from '../components/Home/Home'
+import UserCrud from "../components/Users/UsersCrud";
+
+
+export default props =>
+
+<Routes>
+    <Route exact path="/" element = { <Home /> } />
+    <Route path= "/users" element = { <UserCrud />} />
+    <Route path = "*" element = { <Home />}/>
+</Routes>
